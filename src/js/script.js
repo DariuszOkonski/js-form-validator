9143c83724113f69f98a8dcaa7abbe81df08d7ce
Dariusz Okonski <darek200180@gmail.com>
2020-04-27 20:52:43 +0100

    get all alements

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello world');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const rePassword = document.getElementById('re-password');
+
+function validationSuccess(input) {
+    console.log(input);
+}
+
+function validationError(input) {
+    console.log(input)
+}
+
+
+
+
+
+
+
+
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    
+    
+
+    
+})
